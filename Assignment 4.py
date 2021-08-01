f6b55f9907cef0a515b03c3bd2930e4d7db877e5
JeffersonYarbah <37981517+JeffersonYarbah@users.noreply.github.com>
2021-08-01 17:47:46 +0530

    Final Project

---
+++ Assignment 4.py
@@ -0,0 +1,4 @@
+print("A PYTHON BASED DEGREE CELSIUS TO FAHRENHEIT CONVERTER")
+celsius = int(input("ENTER THE TEMPERATURE IN CELSIUS: "))
+fahrenheit = (celsius * 9/5) + 32
+print("The Converted Temperature is", fahrenheit, "Degree Fahrenheit")
